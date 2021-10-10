c27ab3910dd0ed3087013a297a7360a1573536e4
Hannah-Lisa Whittle <whittlehannahlisa@gmail.com>
2021-10-10 18:34:56 -0500

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,30 @@
+const gameArr = []
+const playerO = "O";
+const playerX = "X";
+// START DEFAULT AS PLAYERX
+let playerTurn = playerX
+function clickedBox(e){
+    const clickedCell = e.target;
+    if (playerTurn === playerX) {
+        clickedCell.classList.add("X");
+        clickedCell.innerHTML = "X";
+        playerTurn = playerO;
+    }
+    else{
+        clickedCell.classList.add("O");
+        clickedCell.innerHTML="O";
+        playerTurn = playerX;
+    }
+    gameArr.push(playerTurn);
+    console.log("button clicked");
+    console.log(gameArr);
+}
+
 window.onload = function (){displayBoard()}
 function displayBoard(){
     var layout = document.getElementById("board").childNodes;
     layout.forEach(elem => {
         elem.className ="square";
+        elem.addEventListener("click",clickedBox);
     });  
 }
